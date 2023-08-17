2ea4e319ea7e0fb60746d1665b6a516870435ac2
Dmitrii Chizhov <dimachig@gmail.com>
2023-08-17 10:16:39 +0300

    init: note-app-2.1

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="New my message"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+//Variables
+$mainFont: 'Montserrat', Helvetica, Arial, sans-serif;
+
+//Mine
+#app {
+  font-family: $mainFont;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
